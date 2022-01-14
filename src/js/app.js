7206c320e215c69ef9764be6ca5d7c85315c9a7f
itrytocodethings <imwaynebailey@gmail.com>
2022-01-14 01:41:10 +0000

    completed conditional profile

--- src/js/app.js
+++ src/js/app.js
@@ -22,30 +22,29 @@ import "../style/index.css";
         city: null
     }
  */
-function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
-}
-
+    function render(variables = {}) {
+      console.log("These are the current variables: ", variables); //print on the console
+      // here we ask the logical questions to make decisions on how to build the html
+      // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+      let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+      if (variables.includeCover == false) cover = "<div class='cover'></div>";
+    
+      // reset the website body with the new html output
+      document.querySelector("#widget_content").innerHTML = `<div class="widget">
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>${!variables.name ? "Your" : variables.name} ${!variables.lastname ? "Name" : variables.lastname}</h1>
+              <h2>${!variables.role ? "Role" : variables.role}</h2>
+              <h3>${!variables.city ? "City" : variables.city}, ${!variables.country ? "Country" : variables.country}</h3>
+              <ul class="${variables.socialMediaPosition}">
+                <li><a href="https://twitter.com/${!variables.twitter ? "4GeeksAcademy" : variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/${!variables.github ? "4GeeksAcademy" : variables.github}"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/${!variables.linkedin ? "4GeeksAcademy" : variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/${!variables.instagram ? "4GeeksAcademy" : variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+        `;
+    }
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
@@ -61,7 +60,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
